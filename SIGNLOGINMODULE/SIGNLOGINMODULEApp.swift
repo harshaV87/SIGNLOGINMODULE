a85068c2c2bd16186f2919d653156f59559b4124
“harshaV87” <harsha.87b@gmail.com>
2023-06-26 15:24:34 -0400

    Initial Commit

---
+++ SIGNLOGINMODULE/SIGNLOGINMODULEApp.swift
@@ -0,0 +1,17 @@
+//
+//  SIGNLOGINMODULEApp.swift
+//  SIGNLOGINMODULE
+//
+//  Created by Venkata harsha Balla on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct SIGNLOGINMODULEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
